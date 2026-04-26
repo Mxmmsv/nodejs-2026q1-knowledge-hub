891933cda6c7cd3aab89d47682fada57662c17f3
Mxm_msv <oikioikioikiabrams@gmail.com>
2026-04-26 14:22:30 +0300

    feat: log sanitized requests and responses

--- src/common/middleware/request-logger.middleware.ts
+++ src/common/middleware/request-logger.middleware.ts
@@ -1,16 +1,29 @@
-import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { AppLoggerService, sanitizeLogData } from '../logger';
 
 @Injectable()
 export class RequestLoggerMiddleware implements NestMiddleware {
-  private readonly logger = new Logger(RequestLoggerMiddleware.name);
+  constructor(private readonly logger: AppLoggerService) {}
 
   use(request: Request, response: Response, next: NextFunction): void {
     const startedAt = Date.now();
+    const url = request.originalUrl ?? request.url;
+
+    this.logger.log('Incoming request', RequestLoggerMiddleware.name, {
+      method: request.method,
+      url,
+      query: sanitizeLogData(request.query),
+      body: sanitizeLogData(request.body),
+    });
 
     response.on('finish', () => {
-      const durationInMs = Date.now() - startedAt;
-      this.logger.log(`${request.method} ${request.originalUrl} ${response.statusCode} ${durationInMs}ms`);
+      this.logger.log('Outgoing response', RequestLoggerMiddleware.name, {
+        method: request.method,
+        url,
+        statusCode: response.statusCode,
+        responseTimeMs: Date.now() - startedAt,
+      });
     });
 
     next();
